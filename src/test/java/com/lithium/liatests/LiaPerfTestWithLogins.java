e594df905acfa26060f1d849fc13efdf21d2c01b
Viren Thakkar <viren.thakkar@lithium.com>
2016-04-22 13:12:29 -0700

    LIA-45461 Update tests for logins with tests

--- src/test/java/com/lithium/liatests/LiaPerfTestWithLogins.java
+++ src/test/java/com/lithium/liatests/LiaPerfTestWithLogins.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by viren.thakkar on 12/23/15.
@@ -30,28 +31,36 @@ public class LiaPerfTestWithLogins {
         int rampup = Integer.parseInt(System.getProperty("rampup"));
         String remoteJmeterInstance = System.getProperty("remoteJmeterHost");
         String csvFile = System.getProperty("csvFileLocation");
-     /*   String elasticSearchCluster = System.getProperty("elasticSearchClusterName");
+        String elasticSearchCluster = System.getProperty("elasticSearchClusterName");
         String elasticSearchHost = System.getProperty("elasticSearchHost");
         int elasticSearchPort = Integer.parseInt(System.getProperty("elasticSearchPort"));
         String testRun = System.getProperty("testRun");
         String release = System.getProperty("release");
         String revision = System.getProperty("revision");
         String community = System.getProperty("community");
+        String code_branch = System.getProperty("codeBranch");
+
 
         /*
           This is to pass extra parameters (JmeterProperties) to jmeter.
          */
-       /* Properties properties = new Properties();
+        Properties properties = new Properties();
         properties.put("elasticSearchClusterName",elasticSearchCluster);
         properties.put("elasticSearchHost",elasticSearchHost);
         properties.put("elasticSearchPort",String.valueOf(elasticSearchPort));
         properties.put("testRun",testRun);
         properties.put("release",release);
         properties.put("revision",revision);
-        properties.put("community",community);*/
+        properties.put("community",community);
+
+        if (code_branch==null || code_branch.isEmpty()) {
+            properties.put("codeBranch", "");
+        }else {
+            properties.put("codeBranch", code_branch);
+        }
 
         JMeterRunner jmeter = new JMeterRunner("httpRequest-warmpup");
-        // jmeter.addExtraJmeterProperties(properties);
+        jmeter.addExtraJmeterProperties(properties);
 
         /* Create header element */
         HeaderManager headerManager = new HeaderManager();
@@ -203,8 +212,8 @@ public class LiaPerfTestWithLogins {
         jmeter.addStep(threadGroup);
 
 
-        // jmeter.addElasticSearchMapping(elasticSearchCluster, elasticSearchHost, elasticSearchPort);
-        //jmeter.addElasticSearchListener(elasticSearchCluster, testRun, elasticSearchHost, elasticSearchPort);
+        jmeter.addElasticSearchMapping(elasticSearchCluster, elasticSearchHost, elasticSearchPort);
+        jmeter.addElasticSearchListener(elasticSearchCluster, testRun, elasticSearchHost, elasticSearchPort);
 
         /* Now run the jmeter client which will run script on remote instance */
         List<String> remoteHosts = Arrays.asList(remoteJmeterInstance.split(","));
--- src/test/java/com/lithium/liatests/Skypec_TestWithUserLogin.java
+++
@@ -1,7 +0,0 @@
-package com.lithium.liatests;
-
-/**
- * Created by viren.thakkar on 3/21/16.
- */
-public class Skypec_TestWithUserLogin {
-}
